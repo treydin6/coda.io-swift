2e266508f309b1411cf294be49008ab0f9903ec5
treydin6 <treydinw@gmail.com>
2024-02-04 23:44:28 -0700

    run swiftLint

--- Sources/Coda/Coda.swift
+++ Sources/Coda/Coda.swift
@@ -1,16 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-
 /// CodaApi is a class for interacting with the Coda API.
 ///
 /// Use this class to create an instance for making requests to the Coda API.
 public final class CodaApi {
-    
+
     /// The API key required for authentication with the Coda API.
     let apiKey: String
-    
-    
+
     /// Initializes a new instance of CodaApi with the provided API key.
     ///
     /// - Parameter apiKey: The API key for authentication.
@@ -19,4 +17,3 @@ public final class CodaApi {
         print("Init Processing: \(apiKey)")
     }
 }
-
