bc0e56862bfe83007f81ad5b9458944325838856
treydin6 <treydinw@gmail.com>
2024-02-04 22:06:44 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Coda
+//
+//  Created by Treydin Winward on 2/4/24.
+//
+
+import Foundation
---
+++ Sources/Coda/Coda.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+class Coda {
+    
+    
+    init(apiKey: String) {
+        print("Init Processing: \(apiKey)")
+    }
+}
