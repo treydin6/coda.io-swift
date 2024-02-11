5b7db64a3bab9fa2c5b33150cae29a494c5701c3
treydin6 <treydinw@gmail.com>
2024-02-10 17:16:58 -0700

    private api key

--- Sources/Coda/Coda.swift
+++ Sources/Coda/Coda.swift
@@ -7,13 +7,12 @@
 public final class CodaApi {
 
     /// The API key required for authentication with the Coda API.
-    let apiKey: String
+    private var apiKey: String
 
     /// Initializes a new instance of CodaApi with the provided API key.
     ///
     /// - Parameter apiKey: The API key for authentication.
     public init(apiKey: String) {
         self.apiKey = apiKey
-        print("Init Processing: \(apiKey)")
     }
 }
