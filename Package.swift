7c234e74cb38d705debfe437b0cf858156020a97
treydin6 <treydinw@gmail.com>
2024-02-11 10:22:42 -0700

    update package version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Coda",
+    platforms: [
+        .macOS(.v10_15), // Set the minimum macOS version
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
