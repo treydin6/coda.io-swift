fda3364f61273ba5273d294a23c29ab298496b2e
treydin6 <treydinw@gmail.com>
2024-02-11 10:21:41 -0700

    removed wrong test

--- Tests/CodaTests/DocsTests/ListAvailableDocsTests.swift
+++ Tests/CodaTests/DocsTests/ListAvailableDocsTests.swift
@@ -11,19 +11,6 @@ import XCTest
 final class ListAvailableDocsTests: XCTestCase {
     
     func testExample() throws {
-        let coda = CodaApi(apiKey: "c408da02-4db3-43cf-9d5e-59ffbe2775b5")
-        var queryParams = ListAvailableDocsParams()
-        queryParams.isOwner = true
-        queryParams.isPublished = true
-        queryParams.queryString = "query string"
-        queryParams.sourceDoc = "source doc"
-        queryParams.isStarred = false
-        queryParams.inGallery = true
-        queryParams.workspaceId = "workspavcedi"
-        queryParams.folderId = "folderid"
-        queryParams.limit = 23
-        queryParams.pageToken = "page token"
-        let res = coda.listAvailabledocs(queryParams: queryParams)
-        print(res)
+
     }
 }
